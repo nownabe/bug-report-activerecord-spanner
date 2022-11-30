301510711d1e2ce49a935e5d0206295b35ab4569
nownabe <nownabe@users.noreply.github.com>
2022-11-30 15:35:03 +0900

    Configure gRPC's timeout

--- example_app/test/models/post_test.rb
+++ example_app/test/models/post_test.rb
@@ -1,5 +1,26 @@
 require "test_helper"
 
+
+# https://github.com/googleapis/google-cloud-ruby/blob/main/google-cloud-spanner-v1/lib/google/cloud/spanner/v1/spanner/client.rb
+Google::Cloud::Spanner::V1::Spanner::Client.configure do |c|
+  timeout_sec = 10 * 365 * 24 * 60 * 60 # ten years
+
+  c.rpcs.create_session.timeout = timeout_sec
+  c.rpcs.batch_create_sessions.timeout = timeout_sec
+  c.rpcs.get_session.timeout = timeout_sec
+  c.rpcs.list_sessions.timeout = timeout_sec
+  c.rpcs.delete_session.timeout = timeout_sec
+  c.rpcs.execute_sql.timeout = timeout_sec
+  c.rpcs.execute_streaming_sql.timeout = timeout_sec
+  c.rpcs.read.timeout = timeout_sec
+  c.rpcs.streaming_read.timeout = timeout_sec
+  c.rpcs.begin_transaction.timeout = timeout_sec
+  c.rpcs.commit.timeout = timeout_sec
+  c.rpcs.rollback.timeout = timeout_sec
+  c.rpcs.partition_query.timeout = timeout_sec
+  c.rpcs.partition_read.timeout = timeout_sec
+end
+
 class PostTest < ActiveSupport::TestCase
   test "now" do
     Post.create!(text: "now")
